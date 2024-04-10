6417efecabd325de5ce53f36d8a3c0ab86a3b71e
Bhendon Soares <bhendon.soares12@gmail.com>
2024-04-10 14:28:38 -0300

    adiciona design pattern: singleton

---
+++ lab-padroes-projeto-java/src/one/digitalinnovation/gof/SingletonEager.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.gof;
+
+/**
+ *  Singleton "apressado"
+ *
+ * @author bhendon-soares
+ */
+
+public class SingletonEager {
+
+    private static SingletonEager instancia = new SingletonEager();
+
+    private SingletonEager() {
+        super();
+    }
+
+    public static SingletonEager getInstancia() {
+        return instancia;
+    }
+}
